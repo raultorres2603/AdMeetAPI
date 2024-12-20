65db264c4c3bc66eafb289b9661a50e41b99b550
Raul Torres <raultorres2603@gmail.com>
2024-12-20 17:24:41 +0100

    logging

--- AdMeet/Services/UserServices.cs
+++ AdMeet/Services/UserServices.cs
@@ -5,30 +5,49 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AdMeet.Services;
 
-public class UserServices(AppDbContext context, IJwt jwt) : IUserServices
+public class UserServices(AppDbContext context, IJwt jwt, Logger<UserServices> logger) : IUserServices
 {
     public async Task<string?> LogIn(User us)
     {
         // We check if user exists
+        logger.LogInformation($"Logging in {us.Email}");
         var user = await context.Users.Include(user => user.Profile).FirstOrDefaultAsync(u => u.Email == us.Email);
         if (user == null)
+        {
+            logger.LogError($"User not found {us.Email}");
             return null;
-        if (BCrypt.Net.BCrypt.EnhancedVerify(us.Password, user.Password)) return jwt.GenerateToken(user);
-        Console.WriteLine($"Password incorrect {us.Password} {user.Password}");
+        }
+
+        if (BCrypt.Net.BCrypt.EnhancedVerify(us.Password, user.Password))
+        {
+            logger.LogInformation($"Password correct {us.Password} {user.Password}");
+            return jwt.GenerateToken(user);
+        }
+
+        logger.LogError($"Password incorrect {us.Password} {user.Password}");
         return null;
     }
 
     public async Task<List<User>> GetUsers()
     {
+        logger.LogInformation("Getting users");
         return await context.Users.ToListAsync();
     }
 
     public async Task<string> Register(User u)
     {
+        logger.LogInformation($"Registering {u.Email}");
         var uExist = await context.Users.FirstOrDefaultAsync(cU => cU.Email!.Equals(u.Email));
-        if (uExist != null) return "UAE";
+        if (uExist != null)
+        {
+            logger.LogError($"User {u.Email} already exists");
+            return "UAE";
+        }
+
         u.Password = u.HashPassword();
+        logger.LogInformation($"Done registering {u.Email}");
         await context.Users.AddAsync(u);
+        logger.LogInformation("Saving changes");
         await context.SaveChangesAsync();
         return "OK";
     }
@@ -37,32 +56,40 @@ public class UserServices(AppDbContext context, IJwt jwt) : IUserServices
     {
         try
         {
+            logger.LogInformation($"Getting info {vJwt}");
             var user = jwt.DecodeToken(vJwt);
+            logger.LogInformation($"Done getting info {vJwt}");
             user = await GetUser(user.Email!);
+            logger.LogInformation($"Done getting info {vJwt}");
             var newTok = jwt.GenerateToken(user);
-            if (newTok == "") throw new Exception("Token not validated");
+            if (newTok == "")
+            {
+                logger.LogError("Token not validated");
+                throw new Exception("Token not validated");
+            }
+
             Console.WriteLine(newTok, user);
             return (user, newTok);
         }
         catch (Exception e)
         {
+            logger.LogError(e.Message);
             throw new Exception(e.Message);
         }
-    }
-
-    public async Task<User> GetUser(string email)
-    {
-        var user = await context.Users.Include(user => user.Profile).FirstOrDefaultAsync(u => u.Email == email);
-        if (user == null) throw new Exception("User not found");
-        return user;
     }
 
     public async Task<string> UpdateProf(User u)
     {
         try
         {
+            logger.LogInformation($"Updating profile {u.Email}");
             var user = await context.Users.Include(user => user.Profile).FirstOrDefaultAsync(u => u.Email == u.Email);
-            if (user == null) throw new Exception("User not found");
+            if (user == null)
+            {
+                logger.LogError("User not found");
+                throw new Exception("User not found");
+            }
+
             user.Profile.Name = u.Profile.Name;
             user.Profile.LastName = u.Profile.LastName;
             user.Profile.City = u.Profile.City;
@@ -70,17 +97,33 @@ public class UserServices(AppDbContext context, IJwt jwt) : IUserServices
             user.Profile.ZipCode = u.Profile.ZipCode;
             try
             {
+                logger.LogInformation("Saving changes");
                 await context.SaveChangesAsync();
                 return "OK";
             }
             catch (Exception e)
             {
+                logger.LogError(e.Message);
                 throw new Exception(e.Message);
             }
         }
         catch (Exception e)
         {
+            logger.LogError(e.Message);
             throw new Exception(e.Message);
         }
+    }
+
+    public async Task<User> GetUser(string email)
+    {
+        logger.LogInformation($"Getting user {email}");
+        var user = await context.Users.Include(user => user.Profile).FirstOrDefaultAsync(u => u.Email == email);
+        if (user == null)
+        {
+            logger.LogError("User not found");
+            throw new Exception("User not found");
+        }
+
+        return user;
     }
 }
